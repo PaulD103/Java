f53d9e0809c3c05e257cc4704f21ef391b84773a
PaulD103 <pauldunz@icloud.com>
2020-03-24 10:43:22 +0100

    adding zelle

---
+++ Mampfi/ZELLE.java
@@ -0,0 +1,24 @@
+class ZELLE{
+    int positionX;
+    int positionY;
+    boolean istMauer;
+    ZELLENSYMBOL zSymbol;
+
+    ZELLE(int xNeu, int yNeu){
+        positionX = xNeu;
+        positionY = yNeu;
+        istMauer = false;
+        zSymbol = new ZELLENSYMBOL(positionX,positionY);
+        zSymbol.FuellFarbeSetzen("blau");
+    }
+
+    void IstMauerSeten(boolean istMauerNeu){
+        istMauer = istMauerNeu;
+        if(istMauer == true){
+            zSymbol.FuellungSichtbarSetzen(true);
+        }
+        else{
+            zSymbol.FuellungSichtbarSetzen(false);
+        }
+    }
+}
